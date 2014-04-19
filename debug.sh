e9c3653a51567ba7b4d70bcc8d9c144f1d56cdbb
Junpei YOSHINO <junpei.yoshino@gmail.com>
2014-04-19 18:53:20 +0900

    run scons debug mode

--- debug.sh
+++ debug.sh
@@ -6,6 +6,6 @@ repo init -u git@github.com:Juniper/contrail-vnc
 repo sync
 python third_party/fetch_packages.py
 (cd third_party/thrift-*; touch configure.ac README ChangeLog; autoreconf --force --install)
-scons --opt=production install --root=${scriptroot}/install-root
+scons .
 cd ${scriptroot}
 
