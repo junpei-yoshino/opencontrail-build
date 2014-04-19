634123bcff19b4f7aa25c538555bc610e0f33863
Junpei YOSHINO <junpei.yoshino@gmail.com>
2014-04-19 18:51:44 +0900

    add requires

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,13 @@ apt-get install libexpat-dev libgettextpo0 libcurl4-openssl-dev
 apt-get install python-dev autoconf automake build-essential libtool
 apt-get install libevent-dev libxml2-dev libxslt-dev
 apt-get install uml-utilities python-lxml redis-server
-apt-get install python-pip
+apt-get install python-pip debhelper javahelper quilt
+apt-get install ant liblog4j1.2-java libhttpcore-java libcommons-codec-java
+
+apt-get install python-software-properties
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-jdk7-installer
 
 # get git-repo (requires git >= 1.7.2)
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
@@ -17,4 +23,5 @@ pip install gevent geventhttpclient==1.0a thrift
 pip install netifaces fabric argparse
 pip install stevedore xmltodict python-keystoneclient
 pip install kazoo pyinotify
+
 
